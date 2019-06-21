4dd6919d2c59b229f935a0bbb681e5750ceb86df
ergusto <fergus.ruston@gmail.com>
2019-06-21 16:09:12 +0100

    Removed unnecessary utility function

--- index.js
+++ index.js
@@ -32,43 +32,6 @@ const start = async () => {
 
 	request = request.defaults({ jar });
 
-	function downloadFile(url) {
-		return new Promise((resolve, reject) => {
-			const file = fs.createWriteStream(config.downloadPath, { flags: "wx" });
-
-			const downloadrequest = request(url, (error, response) => {
-				if (response.statusCode === 200) {
-					response.pipe(file);
-				} else {
-					file.close();
-					fs.unlink(config.downloadPath, () => {}); // Delete temp file
-					reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
-				}
-			});
-
-			downloadrequest.on("error", err => {
-				file.close();
-				fs.unlink(config.downloadPath, () => {}); // Delete temp file
-				reject(err.message);
-			});
-
-			file.on("finish", () => {
-				resolve();
-			});
-
-			file.on("error", err => {
-				file.close();
-
-				if (err.code === "EEXIST") {
-					reject("File already exists");
-				} else {
-					fs.unlink(config.downloadPath, () => {}); // Delete temp file
-					reject(err.message);
-				}
-			});
-		});
-	}
-
 	// Attempt to login
 	try {
 		const loginReq = await request({
